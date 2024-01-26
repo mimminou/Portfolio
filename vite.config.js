a451ae2109a62b29e0dc2005a01f25841b17ff2a
mimminou <mimminou@gmail.com>
2024-01-26 02:53:02 +0100

    updated some dependencies, fixed base url

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    fs:{
-      strict : false
+    fs: {
+      strict: false
     }
   },
-  base: '/Portfolio/',
+  base: '',
   plugins: [svelte(),]
 })
